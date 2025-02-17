7fc176241cae98c8beff4d1a3e761f985ab52cf2
SantaElena89 <72125860+SantaElena89@users.noreply.github.com>
2025-02-17 19:30:33 +0100

    added js file

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-alert("hello world!");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+}
+
+let searchFormElement = document.querySelector(".search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
