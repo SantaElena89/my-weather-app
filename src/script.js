e8db8e6db32e9c40cf5d148e1bf428555ae6e691
SantaElena89 <72125860+SantaElena89@users.noreply.github.com>
2025-12-23 12:10:38 +0100

    updated formatDay function

--- src/script.js
+++ src/script.js
@@ -57,7 +57,7 @@ function handleSearchSubmit(event) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
+  return days[date.getDay];
 }
 
 function getForecast(city) {
@@ -70,11 +70,11 @@ function displayForecast(response) {
   let forecastHtml = "";
   response.data.daily.forEach(function (day, index) {
     if (index < 5) {
-    forecastHtml =
-      forecastHtml +
-      `
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.timestamp)}</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
